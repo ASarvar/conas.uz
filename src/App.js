10146f82e11bc1de40ff6f7b8a68fa4f7654690a
sarvar_alibekov <sarvar_alibekov@student.itpu.uz>
2025-12-23 17:40:10 +0500

    winter bg for header and snowfall

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
       </Switch>
     </Router> */}
       </>
-      {/* <Snowfall
+      <Snowfall
         style={{
           position: "fixed",
           width: "100vw",
@@ -94,7 +94,7 @@ function App() {
         snowflakeCount={300}
         wind={[-0.5, 2.0]}
         radius={[0.5, 2.5]}
-      /> */}
+      />
     </>
   );
 }
